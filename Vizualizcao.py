314244bdc125e803d8622c497be5ea605dfbd213
C0D8 <matheusaguiar362@gmail.com>
2023-03-31 15:04:37 -0300

    Lógica de selecionar apenas um local

--- Vizualizcao.py
+++ Vizualizcao.py
@@ -4,8 +4,11 @@ import sys
 
 
 
+
+
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
+CIANO = (0, 255, 255)
 BLACK = (0, 0, 0)
 
 
@@ -25,8 +28,10 @@ pygame.display.set_caption("Pinte o Tabuleiro")
 
 
 
-
 while True:
+
+
+
     # Lida com eventos do mouse
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,16 +45,25 @@ while True:
             row = y // CELL_SIZE
             
             # Pinta o quadrado clicado
-            board[row][col] = 1
-    
+
+            if board[row][col] != 1:
+            
+                old_cord = np.where(board == 2)
+                board[old_cord] = 0
+                
+                board[row][col] = 2
+
+
+
     # Desenha o tabuleiro na tela
     for row in range(NUM_ROWS):
         for col in range(NUM_COLS):
             if board[row][col] == 1:
                 pygame.draw.rect(screen, BLUE, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            if board[row][col] == 2:
+                pygame.draw.rect(screen, CIANO, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
             else:
-                pygame.draw.rect(screen, WHITE, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
-                
+                pygame.draw.rect(screen, WHITE, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))             
 
     for i in range(1,NUM_COLS):
         pygame.draw.line(screen, color=BLACK, start_pos=(WINDOW_SIZE[0]//NUM_COLS*i,0), end_pos= (WINDOW_SIZE[0]//NUM_COLS*i,WINDOW_SIZE[1]))
@@ -57,6 +71,8 @@ while True:
         pygame.draw.line(screen, color=BLACK, start_pos=(0, WINDOW_SIZE[1]//NUM_ROWS*i), end_pos= (WINDOW_SIZE[0], WINDOW_SIZE[1]//NUM_ROWS*i))
     # Atualiza a tela
     pygame.display.update()
+
+
 
 
 
